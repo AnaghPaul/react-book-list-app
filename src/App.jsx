b963c571ad94a985e81d522ee417693b739f6716
AnaghPaul <125043274+AnaghPaul@users.noreply.github.com>
2025-05-04 15:48:05 +0530

    Adds feature to render one book entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import { Component } from "react"
+import { Component } from "react";
+import "./App.css";
 
-const App = () => {
-  return (<></>)
+class BookEntry extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="book-entry">
+        <p>{this.props.bookTitle}</p>
+        <p>{this.props.author}</p>
+        <p>{this.props.year}</p>
+      </div>
+    );
+  }
 }
 
-export default App
+const App = () => {
+  return (
+    <>
+      <BookEntry
+        bookTitle={"Crime and Punishment"}
+        author={"Fyodor Dostoevsky"}
+        year={1866}
+      />
+    </>
+  );
+};
+
+export default App;
