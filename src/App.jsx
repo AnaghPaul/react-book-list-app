c5eb27796890229870c6f894f25be7bb1d4f463d
AnaghPaul <125043274+AnaghPaul@users.noreply.github.com>
2025-05-04 17:39:18 +0530

    Adds feature to show list of books

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ class BookEntry extends Component {
   render() {
     return (
       <div className="book-entry">
-        <p>{this.props.bookTitle}</p>
+        <p>{this.props.name}</p>
         <p>{this.props.author}</p>
         <p>{this.props.year}</p>
       </div>
@@ -17,15 +17,53 @@ class BookEntry extends Component {
   }
 }
 
+class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [
+        {
+          name: "Crime and Punishment",
+          author: "Fyodor Dostoevsky",
+          year: 1866,
+        },
+        {
+          name: "ABCD",
+          author: "xyz",
+          year: 9232,
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="book-list">
+        {this.state.books.map((book) => {
+          return (
+            <BookEntry name={book.name} author={book.author} year={book.year} />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
 const App = () => {
+  // return (
+  //   <div className="main-container">
+  //     <BookEntry
+  //       name={"Crime and Punishment"}
+  //       author={"Fyodor Dostoevsky"}
+  //       year={1866}
+  //     />
+  //   </div>
+  // );
+
   return (
-    <>
-      <BookEntry
-        bookTitle={"Crime and Punishment"}
-        author={"Fyodor Dostoevsky"}
-        year={1866}
-      />
-    </>
+    <div className="main-container">
+      <BookList />
+    </div>
   );
 };
 
